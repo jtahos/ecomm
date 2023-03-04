d70405a8094e0da6e18251f8d63379988a6473fa
jtahos <jtahos@yahoo.com>
2023-03-03 19:35:00 -0500

    on hoover reads slug but get error when i  try to open page

---
+++ store/urls.py
@@ -0,0 +1,41 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    
+    
+    # Store main page
+    
+    path('', views.store, name='store'),
+    
+    # Individual Product
+    
+    path('product/<slug:product_slug>/', views.product_info, name='product-info'),
+    
+     # Individual Category
+     
+    path('search/<slug:category_slug>/', views.list_category, name='list-category'),
+
+    
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
